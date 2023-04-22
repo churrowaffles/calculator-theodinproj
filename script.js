ad664db8f54d560903576346bd47b91f5cf75620
churrowaffles <106877391+churrowaffles@users.noreply.github.com>
2023-04-22 16:40:58 +0000

    basic calc layout

---
+++ script.js
@@ -0,0 +1,28 @@
+var currentEntry = ''
+var num1 = null
+var num2 = null
+var currentSolution = null
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+})
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(a, b, op) {
+    return op(a, b)
+}
